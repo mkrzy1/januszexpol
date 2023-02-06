67fbf0ef034ce3933aebcb5534d88e7772132f0d
mkrzy1 <magda.krzynowek@gmail.com>
2023-02-06 18:55:22 +0100

    usuwa spacje

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,5 @@ public class Main {
     public static void main(String[] args) {
         BazaPracownikow bazaPracownikow = new LokalnaBazaPracownikow();
         System.out.println(bazaPracownikow.zwrocWynagrodzeniePracownika("Pawel", "Kowalski"));
-
     }
 }
