2bc6f8188b9bac11f45e41defef953ff56301644
mkrzy1 <magda.krzynowek@gmail.com>
2023-02-06 19:07:07 +0100

    Revert "usuwa spacje"
    This reverts commit 67fbf0ef034ce3933aebcb5534d88e7772132f0d.

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         BazaPracownikow bazaPracownikow = new LokalnaBazaPracownikow();
         System.out.println(bazaPracownikow.zwrocWynagrodzeniePracownika("Pawel", "Kowalski"));
+
     }
 }
